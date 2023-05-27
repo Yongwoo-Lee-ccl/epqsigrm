63dfdd35f6eefde0d126e07406f8980ec160cd6c
yongwoo <yongwool@snu.ac.kr>
2023-05-27 10:28:33 +0900

    param.h changed for faster test

--- Reference_Implementation/pqsigrm613/src/parm.h
+++ Reference_Implementation/pqsigrm613/src/parm.h
@@ -4,19 +4,29 @@
 // 
 #ifndef __PARM_H
 #define __PARM_H
-	
-#define RM_R 6U
-#define RM_M 13U
+
+// #define RM_R 6U
+// #define RM_M 13U
+
+// #define K_REP 2U
+
+// #define CODE_K 4096U
+// #define CODE_N 8192U
+
+// #define PARM_P 561U
+
+#define RM_R 5U
+#define RM_M 11U
 
 #define K_REP 2U
 
-#define CODE_K 4096U
-#define CODE_N 8192U
+#define CODE_K 1024U
+#define CODE_N 2048U
 
-#define PARM_P 561U
+#define PARM_P 128U
 
-#define WEIGHT_PUB 1370U    ///
-#define HASHSIZEBYTES 64U  /// 64U
+#define WEIGHT_PUB 300U    
+#define HASHSIZEBYTES 64U 
 
 #define SIGN_FAILURE -1
 #define VERIF_REJECT -1
