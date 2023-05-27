6739bdcaa0c3ac26d2d1122a0dcd16d7cad991de
yongwoo <yongwool@snu.ac.kr>
2023-05-27 16:44:40 +0900

    optimization starts

--- Optimized_Implementation/pqsigrm613/src/parm.h
+++ Optimized_Implementation/pqsigrm613/src/parm.h
@@ -3,33 +3,17 @@
 #ifndef __PARM_H
 #define __PARM_H
 
-// previous parameters
-// #define RM_R 6U
-// #define RM_M 13U
-
-// #define K_REP 2U
-
-// #define CODE_K 4096U
-// #define CODE_N 8192U
-
-// #define PARM_P 561U
-
-// #define WEIGHT_PUB 1370U    
-// previous parameters
-
-// test parameters
-#define RM_R 5U
-#define RM_M 11U
+#define RM_R 6U
+#define RM_M 13U
 
 #define K_REP 2U
 
-#define CODE_K 1024U
-#define CODE_N 2048U
+#define CODE_K 4096U
+#define CODE_N 8192U
 
-#define PARM_P 128U
+#define PARM_P 561U
 
-#define WEIGHT_PUB 512U    
-// test parameters
+#define WEIGHT_PUB 1370U    
 
 #define HASHSIZEBYTES 64U 
 
