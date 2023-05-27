b2aa4a0f9d6fa94c97cf1666566663549799e9f9
yongwoo <yongwool@snu.ac.kr>
2023-05-27 15:54:15 +0900

    revert params.h

--- Optimized_Implementation/pqsigrm613/src/open.c
+++ Optimized_Implementation/pqsigrm613/src/open.c
@@ -10,7 +10,7 @@ crypto_sign_open(unsigned char *m, unsigned long long *mlen,
                  const unsigned char *sm, unsigned long long smlen,
                  const unsigned char *pk){
     matrix *sign = new_matrix(1, CODE_N);
-    matrix *Hpub = new_matrix(CODE_N - CODE_K - 1, CODE_N);
+    matrix *non_identity_hpub = new_matrix(CODE_N - CODE_K - 1, CODE_K + 1);
 
     matrix *syndrome_by_hash = new_matrix(1, CODE_N - CODE_K - 1);
     matrix *syndrome_by_e     = new_matrix(1, CODE_N - CODE_K - 1);
@@ -33,13 +33,20 @@ crypto_sign_open(unsigned char *m, unsigned long long *mlen,
     }
     
     //import public key
-    import_pk(pk, Hpub);
+    import_pk(pk, non_identity_hpub);
 
+    // syndrome by hash
     uint8_t randstr[syndrome_by_hash->ncols/8 + 1];
     hash_message(randstr, m_rx, mlen_rx, sign_i);
     randomize(syndrome_by_hash, randstr);
 
-    vec_mat_prod(syndrome_by_e, Hpub, sign);
+    // syndrome by sign
+    matrix* sign_tail = new_matrix(1, non_identity_hpub->ncols);
+    partial_replace(sign_tail, 0, 1, 0, sign_tail->ncols,
+        sign, 0, non_identity_hpub->nrows);
+    vec_mat_prod(syndrome_by_e, non_identity_hpub, sign_tail);
+    // add sign_head
+    vec_vec_add(syndrome_by_e, sign);
 
     for(uint32_t i=0; i < CODE_N-CODE_K - 1; ++i){
         if(get_element(syndrome_by_hash, 0, i) != get_element(syndrome_by_e, 0, i)){
@@ -51,7 +58,7 @@ crypto_sign_open(unsigned char *m, unsigned long long *mlen,
     *mlen = mlen_rx;
 
     delete_matrix(sign);
-    delete_matrix(Hpub);
+    delete_matrix(non_identity_hpub);
 
     delete_matrix(syndrome_by_hash);
     delete_matrix(syndrome_by_e);
