4163958f3b517f0a1f7b0b8bfe8369c906e3fd28
yongwoo <yongwool@snu.ac.kr>
2023-05-28 07:08:12 +0300

    set to original parameter

--- Optimized_Implementation/pqsigrm613/src/rm.h
+++ Optimized_Implementation/pqsigrm613/src/rm.h
@@ -7,6 +7,8 @@
 
 #include "common.h"
 
+extern const uint16_t rm_dim[7][13];
+
 void rm_gen(matrix* gen, uint32_t r, uint32_t m, 
 	uint32_t row_f, uint32_t row_r, uint32_t col_f, uint32_t col_r);
 
